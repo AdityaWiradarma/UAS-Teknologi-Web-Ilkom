4511bcf3729db6691ee0b5dd8018ff9823d7b3e8
AdityaWiradarma <adityawiradarma03@gmail.com>
2021-05-17 22:39:23 +0700

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,53 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Petfood from '../views/Petfood.vue'
+import PetfoodDetail from '../views/PetfoodDetail.vue'
+import Keranjang from '../views/Keranjang.vue'
+import PesananSukses from '../views/PesananSukses.vue'
+
+Vue.use(VueRouter)
+
+const routes = [{
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/petfood',
+        name: 'Petfood',
+        component: Petfood
+    },
+    {
+        path: '/petfood/:id/',
+        name: 'PetfoodDetail',
+        component: PetfoodDetail
+    },
+    {
+        path: '/keranjang',
+        name: 'Keranjang',
+        component: Keranjang
+    },
+    {
+        path: '/pesanan-sukses',
+        name: 'PesananSukses',
+        component: PesananSukses
+    },
+    {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ '../views/About.vue')
+    }
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
+})
+
+export default router
